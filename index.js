588acb1797dbb0c41ae637c4e6d55be86caf9d5f
fahim-hacker <72152388+fahim-hacker@users.noreply.github.com>
2020-12-31 21:06:50 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+var name = prompt("Write your nick name: ");
+document.getElementById("get-name").innerHTML = "Happy New year " + name;
+document.getElementById("get-na").innerHTML = "Happy New year " + name;
